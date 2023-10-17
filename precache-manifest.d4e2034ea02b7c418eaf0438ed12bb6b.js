53f50d35bae00deabaef4756f83b3f2c58db245f
mohamedshasho <mohamedshasho1@gmail.com>
2023-10-17 23:31:18 +0300

    Updates

---
+++ precache-manifest.d4e2034ea02b7c418eaf0438ed12bb6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f0e95c5d42495cedf2cc8f841766e8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c318446a23ad22ead48",
+    "url": "/portfolio/static/css/2.3aee7458.chunk.css"
+  },
+  {
+    "revision": "1a99cab75502fde71b76",
+    "url": "/portfolio/static/css/main.7cd538dd.chunk.css"
+  },
+  {
+    "revision": "3c318446a23ad22ead48",
+    "url": "/portfolio/static/js/2.c4dc5dd6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.c4dc5dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a99cab75502fde71b76",
+    "url": "/portfolio/static/js/main.570123fa.chunk.js"
+  },
+  {
+    "revision": "a031fe8ca0a902e67d82",
+    "url": "/portfolio/static/js/runtime-main.490b53f3.js"
+  },
+  {
+    "revision": "8e294d0bfb8496857457a7de5d22649c",
+    "url": "/portfolio/static/media/myProfile.8e294d0b.png"
+  }
+]);
